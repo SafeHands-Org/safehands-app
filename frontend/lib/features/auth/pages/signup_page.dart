60b72630fc08da45e24e5f286e3bb8604923ad35
Alexander Gonzalez Ramirez <agonz319@msudenver.edu>
2026-02-02 17:46:10 -0700

    Partial frontend and backend structure set up

---
+++ frontend/lib/features/auth/pages/signup_page.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class SignUpPage extends StatefulWidget {
+  const SignUpPage({super.key});
+
+  @override
+  State<SignUpPage> createState() => _SignUpPageState();
+}
+
+class _SignUpPageState extends State<SignUpPage> {
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  final nameController = TextEditingController();
+
+  @override
+  void dispose(){
+    emailController.dispose();
+    passwordController.dispose();
+    nameController.dispose();
+    super.dispose();
+  }
+  
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text("Sign Up", 
+            style: TextStyle(
+              fontSize: 50,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          TextFormField(
+            controller: emailController,
+          ),
+          TextFormField(
+            controller: passwordController,
+          ),
+          TextFormField(
+            controller: nameController,
+          ),
+          ]
+        ),
+      ),
+    );
+  }
+}
